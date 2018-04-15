b6626a2913c55c347262b3c5e6f92f661a4089f2
Mahmoud Saada <mahmoudsaada@gmail.com>
2018-04-14 22:27:43 -0400

    add css

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react"
 import { render } from "react-dom"
 import Home from "./components/Home"
-import "./style.scss"
 
 render(<Home />, document.getElementById("react-container"))
