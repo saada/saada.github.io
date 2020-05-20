4958cd72724a215dccd1b99f608e0a7e39ebdd07
Mahmoud Saada <mahmoud@weave.works>
2020-05-20 08:55:00 -0400

    update info and styles

--- components/Home.js
+++ components/Home.js
@@ -6,28 +6,40 @@ import imageURL from "../images/mahmoudsaada.jpg"
 const markdown = `
 Mahmoud Saada
 
-![image](${imageURL})
+![landing](${imageURL})
 
 # Work Experience
 
+## [Weaveworks](https://weave.works)
+Customer Reliability Engineer
+
+> Work with customers to select the right technology and introduce a site reliability approach to operating them.
+
 ## [Agolo](https://agolo.com)
-> Site Reliability Engineer
+Site Reliability Engineer
 
-## [Lifion, by ADP](https://lifion.com)
-> Full Stack Software Engineer
+> Establish an SRE team and devops culture to take Agolo towards an immutable infrastructure using Docker and Kubernetes. Influence technical and cultural shifts towards more testing, monitoring, self-service, and automation. Act as a developer-advocate making it easy for dev teams to self-service CI, CD, and automation. Implement monitoring, logging, and automation of software lifecycle for the cloud and on-premises. Manage infrastructure, compute expenses, roadmaps, consultations, and deployments. Contribute to open source projects such as Terraform. Write technical blogs.
 
-Worked full-stack on a metadata-driven platform building core eventbus frameworks integrating services across ADP with Lifion. Developed utility for  in-cloud dev environments used by all engineers to simplify setup, maintenance, and debugging of a 100+ microservice architecture.
+## [Lifion (ADP)](https://lifion.com)
+Full Stack Software Engineer
 
-## [Staffingnation, by TargetCW](https://staffingnation.com)
-> Software Engineer -> Tech Lead
+> Worked full-stack on a metadata-driven platform building core eventbus frameworks integrating services across ADP with Lifion. Developed utility for  in-cloud dev environments used by all engineers to simplify setup, maintenance, and debugging of a 100+ microservice architecture.
 
-Worked on an employee onboarding web app called StaffingNation that  assisted big TargetCW clients, such as Uber, to on-board their employees efficiently through a tailored paperless process. As a startup environment, I was in charge of everything from leading a team of engineers, to writing specifications, to coding across the stack, to building infrastructure, to designing architecture, to making most technical decisions and eventually maintaining it all.
+## [Staffingnation (TargetCW)](https://staffingnation.com)
+Software Engineer &rarr; Tech Lead
+
+> Worked on an employee onboarding web app called StaffingNation that  assisted big TargetCW clients, such as Uber, to on-board their employees efficiently through a tailored paperless process. As a startup environment, I was in charge of everything from leading a team of engineers, to writing specifications, to coding across the stack, to building infrastructure, to designing architecture, to making most technical decisions and eventually maintaining it all.
 
 # Open Source
 
 ## [Container Hive](https://github.com/saada/container-hive)
 
 Visualization tool for Docker container networks
+
+# Certifications
+
+## Certified Kubernetes Administrator
+![cka](https://training.cncf.io/system/files/lfp_cert_badge/logo_cka.png)
 
 # Contact Me
 
